e71c4fb8fe4790a2a4301dae29d1b2b81e01ac37
vitwits <vitwits@gmail.com>
2025-07-15 23:34:05 +0200

    adding count chart for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
 
 const AdminPage = () => {
-    return <div className="p-4 flex gap-4 flex-col md:flex-row">
-        {/* LEFT */}
-        <div className="w-full lg:w-2/3">
-            {/*USER CARDS*/}
-            <div className="flex gap-4 justify-between flex-wrap">
-                <UserCard type="student"/>
-                <UserCard type="teacher"/>
-                <UserCard type="parent"/>
-                <UserCard type="staff"/>
-            </div>
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/*USER CARDS*/}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
-        {/* RIGHT */}
-        <div className="w-full lg:w-1/3">RIGHT</div>
-    </div>;
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHARTS */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANTS  CHARTS */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* BOTTOM CHARTS */}
+        <div></div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">RIGHT</div>
+    </div>
+  );
 };
-
 
 export default AdminPage;
