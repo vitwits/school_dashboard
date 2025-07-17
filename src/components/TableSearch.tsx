27d767941f7b68235e88100ec50b78d0e98585e7
vitwits <vitwits@gmail.com>
2025-07-17 12:30:32 +0200

    added teachers list page on menu

---
+++ src/components/TableSearch.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+const TableSearch = () => {
+  return (
+    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+      <Image src="/search.png" alt="" width={14} height={14} />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-[200px] p-2 bg-transparent outline-none"
+        data-sharkid="__0"
+      />
+    </div>
+  );
+};
+
+export default TableSearch;
