912bedab661400142e094cb279797cb9cbe9fef3
vitwits <vitwits@gmail.com>
2025-07-17 11:03:17 +0200

    added teacher and parent pages

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const ParentPage = () => {
-  return <div>ParentPage</div>;
+  return (
+    <div className="p-4 flex flex-1 gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-xl">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;
